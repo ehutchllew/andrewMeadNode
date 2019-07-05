158d4630df7498ea85a61d48b0d3623e3f9f1b5d
Evan <ehutch7@gmail.com>
2019-07-05 00:04:28 -0400

    Swapped out the array of notes with a map of notes. I convert them to an object to store as JSON data, and then revert them to a map when loading.

--- notesApp/notes.ts
+++ notesApp/notes.ts
@@ -10,9 +10,14 @@ class Notes {
 		/* TODO: Instead of array, make a map and assign
 		 * the key as the title to check for duplicates.
 		 */
-		const allNotes: INote[] = this.loadNotes();
+		const allNotes: Map<string, string> = this.loadNotes();
 
-		allNotes.push({ body, title });
+		if (allNotes.get(title)) {
+			console.error("A note with that title already exists");
+			return;
+		}
+
+		allNotes.set(title, body);
 
 		this.saveNotes(allNotes);
 	}
@@ -21,22 +26,40 @@ class Notes {
 		return "Your notes...";
 	}
 
-	private loadNotes(): INote[] {
+	private loadNotes(): Map<string, string> {
 		try {
 			const dataBuffer: Buffer = fs.readFileSync(this.notesPath);
 
-			const data: any = JSON.parse(dataBuffer.toString());
+			const dataParsed: any = JSON.parse(dataBuffer.toString());
 
-			return data;
+			const dataMap: Map<string, string> = new Map(
+				Object.entries(dataParsed)
+			);
+
+			return dataMap;
 		} catch (error) {
-			console.warn(error);
-			return [];
+			console.warn("**ERROR RETRIEVING NOTES**\n", error);
+			return new Map();
 		}
 	}
 
-	private saveNotes(allNotes: INote[]): void {
-		fs.writeFileSync(this.notesPath, JSON.stringify(allNotes));
+	private saveNotes(allNotes: Map<string, string>): void {
+		const allNotesEntries: IterableIterator<
+			[string, string]
+		> = allNotes.entries();
+
+		let allNotesObject: IMapToObject = {};
+
+		for (const [title, body] of allNotesEntries) {
+			allNotesObject[title] = body;
+		}
+
+		fs.writeFileSync(this.notesPath, JSON.stringify(allNotesObject));
 	}
+}
+
+interface IMapToObject {
+	[key: string]: string;
 }
 
 export { Notes };
