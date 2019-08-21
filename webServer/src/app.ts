5140beca839ffaef845a6d49325f27f848661082
Evan <ehutch7@gmail.com>
2019-08-21 15:53:01 -0400

    Messing around with some routing and templating via HBS

--- webServer/src/app.ts
+++ webServer/src/app.ts
@@ -1,17 +1,37 @@
 import express, { Application } from "express";
+import hbs from "hbs";
 import path from "path";
 
 const app: Application = express();
 const publicPath: string = path.join(__dirname, "../public");
+const viewsPath: string = path.join(__dirname, "../views");
+const partialsPath: string = path.join(__dirname, "../views/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(publicPath));
 
-app.get("/", (req, res) => {
-    res.send(`<h1>Hello World!</h1>`);
+app.get("", (req, res) => {
+    res.render("", {
+        name: "Evan H",
+        title: "Weather App",
+    });
 });
 
-app.get("/weather", (req, res) => {
-    res.send("Weather Page");
+app.get("/about", (req, res) => {
+    res.render("about", {
+        name: "Evander Llew",
+        title: "About Moi",
+    });
+});
+
+app.get("/help", (req, res) => {
+    res.render("help", {
+        name: "Jason Winters",
+        title: "HELP HELP HELP",
+    });
 });
 
 const port: number = 3001;
